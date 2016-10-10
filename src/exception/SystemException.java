cf3bd0a06e94299a2281d8144ee873a7e288f015
matchmen <1142229061@qq.com>
2016-10-10 14:47:07 +0800

    系统异常添加

---
+++ src/exception/SystemException.java
@@ -0,0 +1,52 @@
+package exception;
+
+public class SystemException extends Exception{
+
+	private static final long serialVersionUID = 1L;
+
+	private String pageName;
+	
+	private String errormsg;
+
+	public String getPageName() {
+		return pageName;
+	}
+
+	public void setPageName(String pageName) {
+		this.pageName = pageName;
+	}
+
+	public String getErrormsg() {
+		return errormsg;
+	}
+
+	public void setErrormsg(String errormsg) {
+		this.errormsg = errormsg;
+	}
+
+	public SystemException() {
+		super();
+	}
+
+	public SystemException(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+	public SystemException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public SystemException(String message) {
+		super(message);
+	}
+
+	public SystemException(Throwable cause) {
+		super(cause);
+	}
+	
+	
+	
+	
+	
+}
